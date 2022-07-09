787ef82be453f5cedbdfeb6d5232379351a9379c
Mukesh Kumar Bagaria <mkbagaria13@gmail.com>
2022-07-09 12:43:09 +0530

    navbar changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import NavigationBar from './Component/Navbar/NavigationBar';
 function App() {
   return (
     <div className="App">
-      <NavigationBar/>
+    <NavigationBar/>
     <Chatbot/>
     </div>
   );
